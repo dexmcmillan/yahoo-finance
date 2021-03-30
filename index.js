c05cf7876c59607382bffbfbb78de4c64f360091
dextermcmillan <dexmcmillan@gmail.com>
2021-03-29 22:51:38 -0400

    minor fixes.

--- index.js
+++ index.js
@@ -2,37 +2,42 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function getStockInfo(ticker, exchange) {
-  const html = await axios.get('https://ca.finance.yahoo.com/quote/'+ticker+'.'+exchange+'?p='+ticker+'.'+exchange+'&.tsrc=fin-srch');
-  const holders = await axios.get('https://ca.finance.yahoo.com/quote/' + ticker + '.' + exchange + '/holders?p=' + ticker + '.' + exchange + '')
+  return new Promise(async (resolve, reject) => {
+    const html = await axios.get('https://ca.finance.yahoo.com/quote/'+ticker+'.'+exchange+'?p='+ticker+'.'+exchange+'&.tsrc=fin-srch');
+    const holders = await axios.get('https://ca.finance.yahoo.com/quote/' + ticker + '.' + exchange + '/holders?p=' + ticker + '.' + exchange + '')
 
-  let $ = await cheerio.load(html.data);
+    let $ = await cheerio.load(html.data);
 
-  const info = {
-    insiders: function() {
-    return this.holders['% of Shares Held by All Insider'];
-  }
-  }
+    const info = {
+      ticker: ticker,
+      exchange: exchange,
+      insiders: function() {
+      return this.holders['% of Shares Held by All Insider'];
+    }
+    }
 
-  $('#quote-summary tr').each((i, elem) => {
-    const theKey = $(elem).find('td:nth-child(1)').text().toString()
-    const value = $(elem).find('td:nth-child(2)').text()
+    $('#quote-summary tr').each((i, elem) => {
+      const theKey = $(elem).find('td:nth-child(1)').text().toString()
+      const value = $(elem).find('td:nth-child(2)').text()
 
-    info[theKey] = value
-  });
+      info[theKey] = value
+    });
 
 
-  $ = await cheerio.load(holders.data);
+    $ = await cheerio.load(holders.data);
 
-  info.holders = {}
+    info.holders = {}
 
-  $('section > div:nth-child(2) > div:nth-child(2) tr').each((i, elem) => {
-    const theKey = $(elem).find('td:nth-child(2)').text().toString()
-    const value =  $(elem).find('td:nth-child(1)').text()
+    $('section > div:nth-child(2) > div:nth-child(2) tr').each((i, elem) => {
+      const theKey = $(elem).find('td:nth-child(2)').text().toString()
+      const value =  $(elem).find('td:nth-child(1)').text()
 
-    info.holders[theKey] = value
-  });
+      info.holders[theKey] = value
+    });
 
-  return info
+    return resolve(info)
+  })
+
 }
 
-getStockInfo("TD", "TO").then(response => console.log(response.insiders()))
+module.exports.getStockInfo = getStockInfo
